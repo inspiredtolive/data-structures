91978d8ab559a0a41840638c28e81a9c5740fe7a
inspiredtolive <inspiredtolive0@gmail.com>
2016-07-27 14:47:59 -0700

    Implement functional stack data structure

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,24 @@
 var Stack = function() {
   var someInstance = {};
-
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    storage[size - 1] = value;
   };
 
   someInstance.pop = function() {
+    var popped = storage[size - 1];
+    delete storage[size - 1];
+    size = size === 0 ? size = 0 : size - 1; 
+    return popped;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
