f947471d19b3596fe7997d742d3c572e9e08f8f9
inspiredtolive <inspiredtolive0@gmail.com>
2016-07-27 16:42:40 -0700

    Implement linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    list.tail = Node(value);
+    if (list.head === null) {
+      list.head = list.tail;
+    } else {
+      list.head.next = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    var formerHead = list.head.value;
+    list.head = list.head.next;
+    return formerHead;
   };
 
   list.contains = function(target) {
+    var has = false;
+    var searchNode = function (node) {
+      if (node.value === target) {
+        has = true;
+      } else if (node.next !== null) {
+        searchNode(node.next);
+      }
+    };
+    searchNode(list.head);
+    return has;
   };
 
   return list;
