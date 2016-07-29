3c351dba200d6789a873d5c59c13824a4b313d09
inspiredtolive <inspiredtolive0@gmail.com>
2016-07-28 20:19:33 -0700

    Implement binary search tree with pseudoclassical constructor

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,67 @@
 var BinarySearchTree = function(value) {
+  _.extend(this, Child(value));
 };
 
+BinarySearchTree.prototype.insert = function (val) {
+  var child = Child(val);
+  var searchTree = function (node) {
+    if (node.value > val) {
+      if (node.left === null) {
+        node.left = child;
+      } else {
+        searchTree(node.left);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = child;
+      } else {
+        searchTree(node.right);
+      }
+    }
+  };
+  searchTree(this);
+};
 
+BinarySearchTree.prototype.contains = function (val) {
+  var has = false;
+  var searchTree = function (node) {
+    if (node.value === val) {
+      return has = true;
+    }
+    if (node.value > val) {
+      if (node.left !== null) {
+        searchTree(node.left);
+      }
+    } else {
+      if (node.right !== null) {
+        searchTree(node.right);
+      }
+    }
+  };
+  searchTree(this);
+  return has;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+
+  var iterator = function (node) {
+    callback(node.value);
+    if (node.left) {
+      iterator(node.left);
+    }
+    if (node.right) {
+      iterator(node.right);
+    }
+  };
+  iterator(this);
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+var Child = function(value) {
+  var node = {};
+  node.value = value;
+  node.left = null;
+  node.right = null;
+  return node;
+};
